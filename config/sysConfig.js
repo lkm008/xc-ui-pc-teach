246f12fdb523cd73ab0a942b786112626c6767f2
lkm008 <1524117229@qq.com>
2020-12-18 23:22:55 +0800

    D9:完成课程预览

--- config/sysConfig.js
+++ config/sysConfig.js
@@ -2,7 +2,7 @@ var sysConfig = {
     xcApiUrlPre: '/api',
     //xcApiUrlPre: '',
     xcApiUrl: 'http://api.xuecheng.com',
-    imgUrl:'http://img.xuecheng.com/',
+    imgUrl:'http://img.xuecheng.com:81/',
     videoUrl:'http://video.xuecheng.com',
     openAuthenticate:false,
     openAuthorize:false
